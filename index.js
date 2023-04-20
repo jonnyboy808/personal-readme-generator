ddb147fcd1cb2cc26cdf4ae5be9f92f767854abc
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-19 19:56:12 -0700

    added titling and messages to each prompt

--- index.js
+++ index.js
@@ -4,32 +4,49 @@ const inquirer = require('inquirer');
 const questions = [
     {
         type:'input',
+        name:'title',
+        message:'What should be the title of your project?',
+    },
+   
+    {
+        type:'input',
+        name:'description',
+        message:'Please enter a description of your project',
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'What are the installation steps?',
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'What are the uses for this app?'
+    },
+    {
+        type:'input',
+        name:'contributing',
+        message:'If there were any collaborators or contributors please enter them here.'
+    },
+    {
+        type:'input',
+        name:'test',
+        message:'What are the methods this can be tested?',
+    },
+    {
+        type:'input',
         name:'github',
         message:'What is your github username?',
     },
     {
-        type:
-        name:
-        message:
+        type:'input',
+        name:'email',
+        message:'What is your email address?',
     },
     {
         type:
         name:
         message:
     },
-    {
-        type:
-        name:
-        message:
-    },
-    {
-        type:
-        name:
-        message:
-    },
-    {
-        type:
-        name:
-        message:
-    },
+ 
 ]
