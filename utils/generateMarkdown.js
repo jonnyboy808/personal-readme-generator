620a34d12e2cfa1064296491c6874fef45e402e2
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-20 09:23:03 -0700

    created table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,6 +7,20 @@ function generateMarkdown(data) {
   }
 }
 
+return '# Table of Contents
+1. [Title](#Title)
+2. [Description](#Description)
+3. [Installation](#Installation)
+4. [Usage](#Usage)
+5. [Contributions](#Contributions)
+6. [Test](#Test)
+7. [Github](#Github)
+8. [Email](#Email)
+9. [License](#Licenes)
+
+
+'
+
 
 
 // TODO: Create a function to generate markdown for README
