0173615c3dd14f2124eeab8288a93fb450a7d832
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-19 19:18:02 -0700

    created questions and then replicated for the next 5 prompts

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const questions = [
+    {
+        type:'input',
+        name:'github',
+        message:'What is your github username?',
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+    {
+        type:
+        name:
+        message:
+    },
+]
